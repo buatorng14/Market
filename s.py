d8194882573479e7c61ff1d8b02e562bd2498d6a
buatorng14 <150588311+buatorng14@users.noreply.github.com>
2023-11-22 16:06:19 +0700

    Update s.py

--- s.py
+++ s.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import ast
 import mysql.connector
 # Connect to the MySQL database
 mydb = mysql.connector.connect(
@@ -8,8 +9,35 @@ mydb = mysql.connector.connect(
         password="c757GL28zN",
         database="trong"
 )
-if mydb.is_connected():
-    st.title("pass")  # If connected successfully
-    mycursor = mydb.cursor()
-else :
-    st.title("Connection Error:", err)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT OrderCode, Product, TotalPrice, CustomerNote FROM customer_order")
+data = mycursor.fetchall()
+
+st.title('รายการคำสั่งซื้อทั้งหมด')
+
+for product in data:
+    OrderCode, Product, TotalPrice, CustomerNote = product
+    product_dict = ast.literal_eval(Product)
+    with st.form(key=f'form_{OrderCode}'):
+        st.write(OrderCode)
+        st.write("**สินค้า:**")
+        for item_name, item_quantity in product_dict.items():
+            st.write(f"- {item_name} จำนวน {item_quantity}")
+        st.write(f'**ราคารวม:** :red[{TotalPrice} ฿]')
+        st.write(f'**โน้ตถึงร้านค้า:** {CustomerNote}')
+        
+        # ใส่ st.form_submit_button() ภายในบล็อกของ with st.form()
+        if st.form_submit_button(label='Finish', use_container_width=True):
+            # Insert into history_order table
+            mycursor.execute("INSERT INTO history_order (ordercode) VALUES (%s)", (OrderCode,))
+            mydb.commit()
+            
+            # Delete from orders table
+            mycursor.execute("DELETE FROM customer_order WHERE OrderCode=%s", (OrderCode,))
+            mydb.commit()
+            
+            st.toast(f'Order {OrderCode} deleted successfully! The order is ready for pickup.', icon='❎')
+
+# Close the cursor and database connection outside the loop
+mycursor.close()
+mydb.close()
