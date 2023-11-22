e232cb5c5205fc7a364a36bca58a0a362dd3874f
buatorng14 <150588311+buatorng14@users.noreply.github.com>
2023-11-22 15:23:57 +0700

    Update s.py

--- s.py
+++ s.py
@@ -1,18 +1,17 @@
 import streamlit as st
 import mysql.connector
-
-try:
-    # Connect to the MySQL database
-    mydb = mysql.connector.connect(
+ # Connect to the MySQL database
+mydb = mysql.connector.connect(
         host="139.5.147.31",
         port="3306",
         user="trong",
         password="c757GL28zN",
         database="trong",
         ssl_disabled=True
-    )
-    if mydb.is_connected():
-        st.write("pass")  # If connected successfully
-        mycursor = mydb.cursor()
-except mysql.connector.Error as err:
-    st.write("Connection Error:", err)
+)
+if mydb.is_connected():
+    st.write("pass")  # If connected successfully
+    mycursor = mydb.cursor()
+else:
+    st.write("error)
+    
